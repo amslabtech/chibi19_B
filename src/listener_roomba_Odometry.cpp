94e301491f0303984373cec4a94769326f49130d
Wataru-Y <mechanic1997@gmail.com>
2019-02-15 18:32:08 +0900

    add Odometry

--- src/listener_roomba_Odometry.cpp
+++ src/listener_roomba_Odometry.cpp
@@ -6,20 +6,18 @@
 void chattercallback(const nav_msgs::Odometry::ConstPtr& msg)
 {
 	nav_msgs::Odometry  _msg = *msg;
-	ROS_INFO("pose\n");
-	ROS_INFO("x:%f\n", _msg.pose.pose.position.x);
-	ROS_INFO("y:%f\n", _msg.pose.pose.position.y);
-	ROS_INFO("z:%f\n\n", _msg.pose.pose.position.z);
-	ROS_INFO("orientation\n");
-	ROS_INFO("x:%f\n",_msg.pose.pose.orientation.x);
-	ROS_INFO("y:%f\n",_msg.pose.pose.orientation.y);
-	ROS_INFO("z:%f\n",_msg.pose.pose.orientation.z);
-	ROS_INFO("w:%f\n",_msg.pose.pose.orientation.w);
+	ROS_INFO("p.x:%f", _msg.pose.pose.position.x);
+	ROS_INFO("p.y:%f", _msg.pose.pose.position.y);
+	ROS_INFO("p.z:%f", _msg.pose.pose.position.z);
+	ROS_INFO("o.x:%f",_msg.pose.pose.orientation.x);
+	ROS_INFO("o.y:%f",_msg.pose.pose.orientation.y);
+	ROS_INFO("o.z:%f",_msg.pose.pose.orientation.z);
+	ROS_INFO("o.w:%f",_msg.pose.pose.orientation.w);
 }
 
 int main(int argc, char **argv)
 {
-  ros::init(argc, argv, "listener_roomba_Odmetry");
+  ros::init(argc, argv, "listener_roomba_Odometry");
 
   ros::NodeHandle n;
 
