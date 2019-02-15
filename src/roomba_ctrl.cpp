85fafe6d72af8faf4cf5f6c9503f7aae810526f5
tsuchiya-i <ichiro.code@gmail.com>
2019-02-15 18:05:32 +0900

    ctrl_add_byI

---
+++ src/roomba_ctrl.cpp
@@ -0,0 +1,36 @@
+#include "ros/ros.h"
+#include "roomba_500driver_meiji/RoombaCtrl.h"
+
+#include <sstream>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "roomba_ctrl");
+  ros::NodeHandle n;
+  ros::Publisher ctrl_pub = n.advertise<roomba_500driver_meiji::RoombaCtrl>("roomba/controll", 1000);
+
+  ros::Rate loop_rate(10);
+  int count = 0;
+  while (ros::ok())
+  {
+    roomba_500driver_meiji::RoombaCtrl msg;
+
+    //std::stringstream ss;
+    //ss << "hello world " << count;
+	msg.mode = 11;
+    msg.cntl.liner.x = 0.02;
+
+    ROS_INFO("%d", count);
+
+	ctrl_pub.publish(msg);
+
+    ros::spinOnce();
+
+    loop_rate.sleep();
+    ++count;
+  }
+
+
+  return 0;
+}
+
