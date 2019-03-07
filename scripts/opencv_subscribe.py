395d7052ecd73a5c8b2a9b141054f455e8868494
tsuchiya-i <ichiro.code@gmail.com>
2019-03-07 13:58:48 +0900

    test

--- scripts/opencv_subscribe.py
+++ scripts/opencv_subscribe.py
@@ -15,19 +15,19 @@ def callback(data):
     cv2.imshow("original",img)
 
 
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    ret, img_threshold = cv2.threshold(img_gray,200,255,cv2.THRESH_BINARY)
-    img_canny = cv2.Canny(img_threshold, 50, 110)
-
-    lines = cv2.HoughLinesP(img_canny, rho=1, theta=np.pi/360, threshold=30, minLineLength=200, max LineGap=20)
-    print(lines)
-    for line in lines:
-        x1, y1, x2, y2 = line[0]
-        #redline
-        img_red_line = cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 3)
-
-
-    cv2.imshow("red_line", img_red_line)
+#    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#    ret, img_threshold = cv2.threshold(img_gray,200,255,cv2.THRESH_BINARY)
+#    img_canny = cv2.Canny(img_threshold, 50, 110)
+#
+#    lines = cv2.HoughLinesP(img_canny, rho=1, theta=np.pi/360, threshold=30, minLineLength=200, max LineGap=20)
+#    print(lines)
+#    for line in lines:
+#        x1, y1, x2, y2 = line[0]
+#        #redline
+#        img_red_line = cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 3)
+#
+#
+#    cv2.imshow("red_line", img_red_line)
     cv2.waitKey(3)
 
 
