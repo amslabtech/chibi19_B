de11d99c4229c231edc53a4db1bd46cb6a15d9a1
tsuchiya-i <ichiro.code@gmail.com>
2019-03-07 12:05:40 +0900

    red_line

--- scripts/opencv_subscribe.py
+++ scripts/opencv_subscribe.py
@@ -1,23 +1,53 @@
-import rospy
-from std_msgs.msg import String
+#!/usr/bin/env python 
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.data)
+import rospy
+import numpy as np
+import cv2
+#from sensor_.msgs import Image
+
+#def callback(data):
+#    rospy.loginfo(rospy.get_caller_id()+"I heard %s",data.encoding)
+#
+#    cv2.imshow("input", data.data)
+#
+#    cv2.waitKey(0)
+    #cv2.destroyAllWindows()
     
 def listener():
+     #cv2.namedWindow('img', cv2.WINDOW_NORMAL)
 
-    # in ROS, nodes are unique named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The 
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaenously.
-    rospy.init_node('listener', anonymous=True)
+    img = cv2.imread('white_line.png') 
+    cv2.imshow("original", img)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    ret, img_threshold = cv2.threshold(img_gray,200,255,cv2.THRESH_BINARY)
+    img_canny = cv2.Canny(img_threshold, 50, 110)
 
-    rospy.Subscriber("chatter", String, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
+    lines = cv2.HoughLinesP(img_canny, rho=1, theta=np.pi/360, threshold=30, minLineLength=200, maxLineGap=20)
+    print(lines)
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        #redline
+        img_red_line = cv2.line(img, (x1,y1), (x2,y2), (0,0,255), 3)
+
+
+    #cv2.imshow("grayscale", img_gray)
+    #cv2.imshow("threshold", img_threshold)
+    #cv2.imshow("canny", img_canny)
+    cv2.imshow("red_line", img_red_line)
+
+
+    #cv2.imwrite("./output/img_otsu.jpg",img_otsu)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    #print('Hello!!!!!!!!!!!!!')
+    
+    #rospy.init_node('opencv_subscribe', anonymous=True)
+    #rospy.Subscriber("usb_cam/image_raw", Image, callback)
+   # rospy.spin()
         
 if __name__ == '__main__':
     listener()
+
 
