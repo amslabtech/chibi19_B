d3c040744ec44c40322a71276712d4ef1f7505d6
Wataru-Y <mechanic1997@gmail.com>
2019-03-19 14:46:34 +0900

    bug fix

--- src/Localization.cpp
+++ src/Localization.cpp
@@ -136,7 +136,7 @@ int get_index(double x, double y)
 
 bool update_judge(geometry_msgs::PoseStamped current, geometry_msgs::PoseStamped previous)
 {
-	if(sqrt(pow(current.pose.position.x,2)+pow(current.pose.position.y-previous.pose.position.y,2))<0.01)
+	if(sqrt(pow(current.pose.position.x-previous.pose.position.x,2)+pow(current.pose.position.y-previous.pose.position.y,2))<0.01)
 	{
 		return false;
 	}
@@ -197,15 +197,15 @@ int main(int argc, char** argv)
 			current_pose.pose.position.y = transform.getOrigin().y();
 			quaternionTFToMsg(transform.getRotation(), current_pose.pose.orientation);
 
-			if(update_judge(current_pose, previous_pose))
-			{
-				ROS_INFO("motion update/n");
+			/*if(update_judge(current_pose, previous_pose))
+			{*/
+				//ROS_INFO("motion update/n");
 
 				for(int i=0;i<N;i++)
 				{
 					Particles[i].motion_update(current_pose, previous_pose, i);
 				}
-			}
+			//}
 
 			estimated_pose = current_pose;
 			geometry_msgs::PoseWithCovarianceStamped _estimated_pose;
