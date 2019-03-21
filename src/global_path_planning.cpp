494fcd4292dab25656b99c3bd8c0c29cd07c41e7
tsuchiya-i <ichiro.code@gmail.com>
2019-03-22 00:51:55 +0900

    経路の配列追加

--- src/global_path_planning.cpp
+++ src/global_path_planning.cpp
@@ -44,7 +44,7 @@ int min_array(const int *a, int size,int &min){ //<<positive value
 			min_i=i;
 			break;
 		}
-		if(i==size-1) printf("error\n");
+		if(i==size-1) min = -1;//printf("error\n");
 	}
 	
 	for(int i=0;i<size;i++){
@@ -109,7 +109,7 @@ void show_farray(float array[row][column]){
      printf("]\n");
 }
 
-void show_path(int array[row][column]){
+void show_cost_grid(int array[row][column]){
 	for(int i=0;i<row;i++){
 		for(int j=0;j<column;j++){
 			if(array[i][j] == -1) printf("■ ");
@@ -120,40 +120,42 @@ void show_path(int array[row][column]){
 	printf("\n");
 }
 
-
-
-float search(const int grid[row][column],const int heuristic_grid[row][column],
-	float path[row][column], int result_path[row][column],
-	const int init[2],const int goal[2])
+int search(const int grid[row][column],const int heuristic_grid[row][column],
+	float cost_grid[row][column], int temporary_path[][3], const int init[2],const int goal[2])
 {
 	int move_history[row*column][2];
 	int selected_move = 0;
 	int a[direction]= {-1};
+	int eight_cost[direction];
 	int y = init[0];
 	int x = init[1];
-	int d;
+	int d,min_c,step,home;
 	int move_direction;
-	int f;
-	float total_cost=0.0;
 
-	set_all(result_path,-1);
-	
-	path[y][x] = 0;
-	for(f=1;f<row*column;f++){
+	cost_grid[y][x] = 0;
+	for(int f=0;f<row*column;f++){
 		if(x == goal[1] && y == goal[0]){
-			printf("success!!\n");
-			break;
+			cost_grid[y][x] = step+1;
+			temporary_path[step+1][0] = y;
+			temporary_path[step+1][1] = x;
+			temporary_path[step+1][2] = move_direction;
+			//printf("success!!\n");
+			return cost_grid[y][x];
 		}
 		for(int i=0;i<direction;i++){
 			if(-1<(x+delta[i][1]) && column>(x+delta[i][1]) && -1<(y+delta[i][0]) && row>(y+delta[i][0])){
+				eight_cost[i] = cost_grid[y+delta[i][0]][x+delta[i][1]];
 				if(grid[y+delta[i][0]][x+delta[i][1]] != 1){
-					if(path[y+delta[i][0]][x+delta[i][1]] < 0)
+					if(cost_grid[y+delta[i][0]][x+delta[i][1]] < 0)
 						a[i] = heuristic_grid[y+delta[i][0]][x+delta[i][1]];
 					else a[i] = -1;
 				}
 				else a[i] = -1;
 			}
-			else a[i] = -1;
+			else {
+				a[i] = -1;
+				eight_cost[i]=-1;
+			}
 		}
 		
 		if(positive_count(a,direction)>1){
@@ -166,53 +168,64 @@ float search(const int grid[row][column],const int heuristic_grid[row][column],
 		if(!positive_count(a,direction)){
 			if(!selected_move){
 				printf("----analysis is impossible----\n");
-				break;
+				return 0;
 			}
 			selected_move--;
 			y = move_history[selected_move][0];
 			x = move_history[selected_move][1];
-			f--;
 		}
 		
 		else{
+			home = min_array(eight_cost,direction,min_c);//from home
+			step = min_c+1;
+			if(step>0) temporary_path[step-1][2] = (home+4)%9;
+			cost_grid[y][x] = step;
 			move_direction = min_array(a,direction,d);
-			result_path[y][x] = move_direction;
+			temporary_path[step][0] = y;
+			temporary_path[step][1] = x;
+			temporary_path[step][2] = move_direction;
 			x = x+delta[move_direction][1];
 			y = y+delta[move_direction][0];
-			total_cost += delta_cost[move_direction]; 
-			path[y][x] = total_cost;
 		}
 	}
-	f--;
-	return total_cost;
 }
 
+int set_path(int temporary_path[][3], int result_path[][3],int step)
+{
+	for(int i=0;i<step;i++){
+		result_path[i][0] = temporary_path[i][0];
+		result_path[i][1] = temporary_path[i][1];
+		result_path[i][2] = temporary_path[i][2];
+		printf("path = [%3d,%3d,%3d]\n",result_path[i][0],result_path[i][1],result_path[i][2]);
+	}
+
+}
+	
 
 int main(void){
 	
 	int grid[row][column] = {{0, 1, 0, 0, 0, 0},
-							 {0, 1, 0, 1, 1, 0},
 							 {0, 0, 0, 1, 1, 0},
+							 {0, 1, 0, 1, 1, 0},
 							 {1, 1, 0, 1, 1, 0},
 							 {0, 0, 0, 0, 1, 0}};
 	
 
 	int heuristic_grid[row][column];
-	float path[row][column];
-	int result_path[row][column];
-	fset_all(path,-1);
+	float cost_grid[row][column];
+	int temporary_path[row*column][3];
+	fset_all(cost_grid,-1);
 	
 	const int init[2] = {0,0};
 	const int goal[2] = {4,5};
-	//int cost = 1;
 	
 	set_heuristic(heuristic_grid,goal);
 	
-	float cost = search(grid, heuristic_grid,path,result_path,init,goal);
+	const int step = search(grid, heuristic_grid,cost_grid,temporary_path,init,goal) + 1;
+	int result_path[step][3];
+
+	set_path(temporary_path, result_path, step);
 	printf("grid = \n"); show_array(grid);
-	printf("heuristic_grid = \n"); show_array(heuristic_grid);
-	printf("path = \n"); show_farray(path);
-	printf("total_cost = %f\n",cost);
-	printf("result_path = \n"); show_array(result_path);
-	show_path(result_path);
+	//printf("heuristic_grid = \n"); show_array(heuristic_grid);
+	printf("cost_grid = \n"); show_farray(cost_grid);
 }
