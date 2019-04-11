0f1b12b0a3790e52d893f9b809b613b1a0f21a86
ShimadaKota <kota6011@gmail.com>
2019-04-11 13:20:47 +0900

    dwa

--- src/DWA.cpp
+++ src/DWA.cpp
@@ -17,8 +17,6 @@
 #define to_goal_cost_gain 0.0
 #define speed_cost_gain 0.0
 #define robot_radius 0.19
-#define roomba_v_gain 20
-#define roomba_omega_gain 0.5
 
 const int N = 720;//(_msg.angle_max - _msg.angle_max) / _msg.angle_increment;
 
@@ -79,7 +77,11 @@ void calc_trajectory(std::vector<State> &traj, double i, double j){
 	traj.clear();
 
 	for(double t = 0.0; t <= predict_time; t += dt){
-		motion(roomba, u);
+		roomba.yaw += u.omega * dt;
+		roomba.x += u.v * std::cos(roomba.yaw) * dt;
+		roomba.y += u.v * std::sin(roomba.yaw) * dt;
+		roomba.v = u.v;
+		roomba.omega = u.omega;
 		traj.push_back(roomba);
 	}
 }
@@ -164,7 +166,6 @@ void calc_final_input(State roomba, Speed &u, double dw[4], Goal goal){
 			calc_trajectory(traj, i, j);
 			to_goal_cost = calc_to_goal_cost(traj, goal);
 			speed_cost = calc_speed_cost(traj);
-			ROS_INFO("1\n");
 			ob_cost = calc_obstacle_cost(roomba, traj, goal);
 
 			final_cost = to_goal_cost + speed_cost + ob_cost;
@@ -230,8 +231,8 @@ int main(int argc, char **argv)
 	roomba.v = u.v;
 	roomba.omega = u.omega;
 
-	msg.cntl.linear.x = roomba.v * roomba_v_gain;
-	msg.cntl.angular.z = roomba.omega * roomba_omega_gain;
+	msg.cntl.linear.x = roomba.v / max_speed;
+	msg.cntl.angular.z = roomba.omega / max_yawrate;
 
 	//check goal
 /*	if(sqrt(pow(roomba.x - goal.x, 2.0) + pow(roomba.y - goal.y, 2.0)) < robot_radius){
