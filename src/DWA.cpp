d55bbb66f0ae05a9fd909a7f625263de1f4ca7e9
ShiamdaKota <kota6011@gmail>
2019-04-16 15:21:04 +0900

    src

--- src/DWA.cpp
+++ src/DWA.cpp
@@ -206,7 +206,7 @@ void calc_final_input(State roomba, Speed& u, Dynamic_Window& dw, Goal goal){
 		for(double j = dw.min_omega ; j < dw.max_omega ; j += yawrate_reso){
 			calc_trajectory(traj, roomba,  i, j);
 			to_goal_cost = calc_to_goal_cost(traj, goal);
-			//speed_cost = calc_speed_cost(traj);
+			speed_cost = calc_speed_cost(traj);
 			//ob_cost = calc_obstacle_cost(roomba, traj, goal);
 
 			final_cost = to_goal_cost + speed_cost + ob_cost;
