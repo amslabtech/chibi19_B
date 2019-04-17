7a9b2576d84b2c6a57567f96662ba558b508692f
ShimadaKota <kota6011@gmail.com>
2019-04-17 19:55:59 +0900

    src

--- src/DWA.cpp
+++ src/DWA.cpp
@@ -222,6 +222,7 @@ void calc_final_input(State roomba, Speed& u, Dynamic_Window& dw, Goal goal){
 	}
 
 	ROS_INFO("goal_cost = %f, speed_cost = %f, obstacle_cost = %f, final cost = %f", to_goal_cost, speed_cost, ob_cost, final_cost);
+	ROS_INFO("i = %f, j = %f", min_u.v, min_u.omega);
 	u = min_u;
 }
 
