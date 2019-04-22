1b6deaacec0487da45598934d324471848518f29
ShiamdaKota <kota6011@gmail>
2019-04-22 16:33:37 +0900

    src

--- src/DWA.cpp
+++ src/DWA.cpp
@@ -369,7 +369,8 @@ int main(int argc, char **argv)
 	//ROS_INFO("roomba.x = %f, roomba.y = %f, roomba.yaw = %f", roomba.x, roomba.y, roomba.yaw);
 	//ROS_INFO("goal.x = %f, goal.y = %f", goal.x, goal.y);
 	//ROS_INFO("x = %f, z = %f", msg.cntl.linear.x, msg.cntl.angular.z);
-	loop_rate.sleep();
+	ROS_INFO("v = %f, omega = %f", roomba.v, roomba.omega);
+oop_rate.sleep();
 	}
 	
 	return 0;
