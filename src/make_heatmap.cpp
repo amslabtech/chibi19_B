2d92841f73bb3a7f3f47b725ca08b244f120d916
tsuchiya-i <ichiro.code@gmail.com>
2019-04-24 12:13:06 +0900

    サーモグラフィみたいに

--- src/make_heatmap.cpp
+++ src/make_heatmap.cpp
@@ -20,11 +20,12 @@ int grid[row][column];
 float wallcost_grid[row][column];
 
 void set_wallcost(float array[row][column]){
-    int range = 35;
+    int range = 20;
 	float near_kl = 0;
 	float far = sqrt((range/2)*(range/2)*2);
 	int count = 0;
-	int adjust = 3;
+	int adjust = 0;
+	float max_value = 1.0;
     for(int i=0;i<row;i++){
          for(int j=0;j<column;j++){
              if(grid[i][j] == 0){
@@ -40,16 +41,32 @@ void set_wallcost(float array[row][column]){
 						}
                      }
                  }
-				 array[i][j] = (255.0/(far+adjust)) * (near_kl+adjust);
+				 array[i][j] = max_value - (max_value/(far+adjust)) * (near_kl+adjust);
              }
-			 else array[i][j] = 0;
+			 else array[i][j] = 10.0;
          }
      }
 	ROS_INFO("c = %d",count);
 }
 
+
+void ColorScaleBCGYR( double in_value, int &r,int &g,int &b)
+{
+    // 0.0～1.0 の範囲の値をサーモグラフィみたいな色にする
+    double  value = in_value;
+    double  tmp_val = cos( 4 * M_PI * value );
+    int     col_val = (int)( ( -tmp_val / 2 + 0.5 ) * 255 );
+         if ( value >= ( 4.0 / 4.0 ) ) { r = 255;     g = 0;       b = 0;       }   // 赤
+    else if ( value >= ( 3.0 / 4.0 ) ) { r = 255;     g = col_val; b = 0;       }   // 黄～赤
+    else if ( value >= ( 2.0 / 4.0 ) ) { r = col_val; g = 255;     b = 0;       }   // 緑～黄
+    else if ( value >= ( 1.0 / 4.0 ) ) { r = 0;       g = 255;     b = col_val; }   // 水～緑
+    else if ( value >= ( 0.0 / 4.0 ) ) { r = 0;       g = col_val; b = 255;     }   // 青～水
+    else {                               r = 0;       g = 0;       b = 255;     }   // 青
+}
+
 void make_heatmap(float array[row][column])
 {
+	int b,g,r;
 	const int size = row+1;
 	cv::Mat hsv_image(row,column,CV_8UC3);
 	cv::Mat thsv_image(row,column,CV_8UC3);
@@ -57,8 +74,12 @@ void make_heatmap(float array[row][column])
 
 	for(int i = 0;i<row;i++){
 		for(int j=0;j<column;j++){
-			if(array[i][j] == 0) hsv_image.at<cv::Vec3b>(i,j) = cv::Vec3b(100,100,100); 
-			else hsv_image.at<cv::Vec3b>(i,j) = cv::Vec3b(array[i][j],0,255-array[i][j]); //bgr
+			if(array[i][j] > 5) hsv_image.at<cv::Vec3b>(i,j) = cv::Vec3b(100,100,100); 
+			else{
+				ColorScaleBCGYR(array[i][j],r,g,b);
+				hsv_image.at<cv::Vec3b>(i,j) = cv::Vec3b(b,g,r); //bgr
+			}
+			//else hsv_image.at<cv::Vec3b>(i,j) = cv::Vec3b(array[i][j],0,255-array[i][j]); //bgr
 			//else hsv_image.at<cv::Vec3b>(i,j) = cv::Vec3b(array[i][j],array[i][j],array[i][j]);
 		}
 	}
