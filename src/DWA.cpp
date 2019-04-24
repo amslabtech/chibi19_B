b43f120e1b89fd3c2af7f7c1350d47d52fa2112a
ShiamdaKota <kota6011@gmail>
2019-04-24 12:14:39 +0900

    src

--- src/DWA.cpp
+++ src/DWA.cpp
@@ -261,7 +261,7 @@ void calc_final_input(State roomba, Speed& u, Dynamic_Window& dw, Goal goal){
 		for(double j = dw.min_omega ; j < dw.max_omega ; j += yawrate_reso){
 			calc_trajectory(traj, roomba,  i, j);
 			to_goal_cost = calc_to_goal_cost(traj, goal, roomba);
-			goal_dist = 3.0 *  calc_goal_dist(traj, goal);
+			goal_dist = 4.0 *  calc_goal_dist(traj, goal);
 			speed_cost = calc_speed_cost(traj);
 			ob_cost = calc_obstacle_cost(roomba, traj);
 
@@ -369,16 +369,10 @@ int main(int argc, char **argv)
 	
 	//yaw = roomba.yaw;
 	while(white_line_detector == true){
-	  ROS_INFO("white = %d", white_line_detector);
-	  msg.cntl.angular.x = 0.0;
-	  msg.cntl.angular.z = 0.5;
-	  
-	  if(fabs(tf::getYaw(est_pose_msg.pose.pose.orientation) - roomba.yaw) < 0.05){
-		msg.cntl.angular.x = 0.5;
-		msg.cntl.angular.z = 0.0;
-		sleep(2);
-		break;
-	  }
+	  msg.cntl.linear.x = 0.0;
+	  msg.cntl.angular.z = 0.0;
+	  sleep(5);
+	  break;
 	}
 
 	if(dist == false && roomba.y > 2.0){
